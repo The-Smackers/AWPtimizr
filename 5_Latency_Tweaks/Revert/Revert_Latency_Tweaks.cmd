1d9812583b01971bdfe0c448eb94fd2fc712345c
pdq <idk@users.noreply.github.com>
2025-04-08 07:43:55 -0500

    Delete 5_Latency_Tweaks directory

--- 5_Latency_Tweaks/Revert/Revert_Latency_Tweaks.cmd
+++
@@ -1,9 +0,0 @@
-@echo off  
-echo Enable Dynamic Tick
-echo Enable High Precision Event Timer (HPET)
-echo Enable Synthetic Timers
-@echo 
-bcdedit /deletevalue disabledynamictick
-bcdedit /set useplatformclock yes
-bcdedit /deletevalue useplatformtick
-pause
